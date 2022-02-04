e8f6a923175962ca96f2c5c13d8ec6e6b243d250
Danylo Donets <EndoBroYT@gmail.com>
2022-02-04 15:56:28 +0200

    Add files via upload

--- src/TestResult.java
+++ src/TestResult.java
@@ -1,12 +1,16 @@
 //package test;
-
+import java.util.Scanner;  
 //import domain.Exercise;
 
 public class TestResult {
 
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        Scanner console = new Scanner(System.in); 
+        System.out.println("First string:");
+        String s1 = console.nextLine();
+        System.out.println("Second string:");
+        String s2 = console.nextLine();
+        console.close();
+        System.out.println("Anagram = "+Exercise.Calculate(s1,s2));
     }
 }
